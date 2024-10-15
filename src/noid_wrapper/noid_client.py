34cea76a29e3ec18ee8de5001250d4595821b013
Stephen Appel <srappel930@gmail.com>
2024-10-15 11:24:28 -0500

    get minter working

--- src/noid_wrapper/noid_client.py
+++ src/noid_wrapper/noid_client.py
@@ -7,7 +7,7 @@ class NoidClient:
     def __init__(self, config_path="config.yaml"):
         self.config = self._load_config(config_path)
         self.noid_path = self.config["NOID"].get("noid_path", "noid")
-        self.namespace = self.config["NOID"].get("namespace", "ark:/77981/gmgs")
+        self.db_path = self.config["NOID"].get("db_path", ".")
         self._setup_logging()
 
     def _load_config(self, config_path):
@@ -19,10 +19,10 @@ class NoidClient:
         logging.basicConfig(level=getattr(logging, log_level))
         self.logger = logging.getLogger(__name__)
 
-    def mint(self):
-        """Mint a new ARK."""
+    def mint(self, count=1):
+        """Mint a specified number of ARKs. Default is 1."""
         self.logger.info("Minting a new ARK...")
-        return self._run_noid_command("mint", self.namespace)
+        return self._run_noid_command("mint", str(count))
 
     def bind(self, ark, value):
         """Bind a value to an existing ARK."""
@@ -31,7 +31,7 @@ class NoidClient:
 
     def _run_noid_command(self, *args):
         """Run a NOID command using subprocess."""
-        command = [self.noid_path] + list(args)
+        command = [self.noid_path, '-f', self.db_path] + list(args)
         self.logger.debug(f"Running command: {' '.join(command)}")
         try:
             result = subprocess.run(command, capture_output=True, text=True, check=True)
@@ -39,3 +39,8 @@ class NoidClient:
             self.logger.error(f"NOID command failed: {e.stderr}")
             raise
         return result.stdout
+
+if __name__ == '__main__':
+    client = NoidClient()
+    ark = client.mint(5)
+    print(f"Minted ARK(s): {ark}")
